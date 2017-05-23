b4036d0c17cb01bca97cf3bcdec4836ee6c5dc37
Mariusz Maciejczuk <mariusz.maciejczuk@hotmail.com>
2017-05-23 23:31:23 +0200

    adding views and styles

---
+++ OGL2/Controllers/WiadomoscController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OGL2.Controllers
+{
+    public class WiadomoscController : Controller
+    {
+        // GET: Wiadomosc
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
